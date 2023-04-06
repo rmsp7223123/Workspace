3be0ec4a9622e2477463ace88a7452df3abab7f5
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-04-06 09:17:08 +0900

    수정

--- 04.ControlStatement/src/pack03_For/Ex04_ForFor.java
+++ 04.ControlStatement/src/pack03_For/Ex04_ForFor.java
@@ -29,10 +29,11 @@ public class Ex04_ForFor {
 		Scanner sc = new Scanner(System.in);
 		sc.nextLine();
 		
-		for (int a = 1; a <= 9; a++) {
-			for (int b = 2; b<= 9; b++) {
-				System.out.println(a * b);
+		for (int a = 2; a <= 9; a++) {
+			for (int b = 1; b<= 9; b++) {
+				System.out.print(a + " X " + b + " = " + (a * b) +" ");
 			}
+			System.out.println();
 		}
 		
 	}
