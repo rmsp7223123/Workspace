8238fbf640aeb0df97728b97c1b7f4a29090e22b
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-04-21 17:56:21 +0900

    +

---
+++ 01.HelloWorld/src/JapangiDTO.java
@@ -0,0 +1,13 @@
+package pack01_japangi;
+
+public class JapangiDTO {
+	String drink;
+	int drinkPrice, inv;
+	
+	public JapangiDTO(String drink, int drinkPrice, int inv) {
+		this.drink = drink;
+		this.drinkPrice = drinkPrice;
+		this.inv = inv;
+	}
+}
+
