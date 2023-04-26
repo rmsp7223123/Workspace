a4bc60e4f7e30c81a8f6173cdee7c62b99f126f9
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-04-26 18:28:02 +0900

    ++

---
+++ 999.Test/src/lotto/lottoMain.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import java.util.Random;
+
+public class lottoMain {
+	public static void main(String[] args) {
+		lottoDTO dto = new lottoDTO();
+		int lottoNum = new Random().nextInt(45)+1;
+		
+	}
+}
