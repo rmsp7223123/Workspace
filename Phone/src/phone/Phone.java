090f8e973f3d45527b797a67b58f541e14932d6f
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-04-27 13:56:51 +0900

    ++

--- Phone/src/phone/Phone.java
+++ Phone/src/phone/Phone.java
@@ -1,6 +1,35 @@
 package phone;
 
+import java.util.Scanner;
+
 public class Phone {
+	Scanner sc = new Scanner(System.in);
+	String model;
+	String state;
+	String color;
+	String power;
+	public Phone(String model, String color) {
+		this.model = model;
+		this.color = color;
+	}
+
+	public void stateOff() {
+		System.out.println("전원을 켜시려면 poweron을 입력해주세요.");
+		while (true) {
+			power = sc.nextLine();
+			if (power.equals("poweron")) {
+				System.out.println("전원이 켜졌습니다.");
+				System.out.println("원하시는 기능을 선택해주세요.");
+				System.out.println("1번 = 전화걸기, 2번 전원종료");
+			} else {
+				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+			}
+		}
+	}
+
+	public void stateOn() {
+
+	}
 //	-기능1: 전원 켜기 & 끄기 : return 이 없는 기능으로 단순하게 전원이 켜고 꺼지며 , 상태 정보가 변경 된다
 //		    (ex . 전원 켜기 : 상태 ( 전원 켜짐 ) , 전원 끄기 ( 전원 꺼짐 )
 //		    
@@ -10,6 +39,5 @@ public class Phone {
 //	                       
 //	-기능3: 음성 전송 & 수신 : 전화중 상태에서만 음성 전송 및 수신이 가능하며 문자열을 입력하여 음성을 전송하고 문자열을
 //              입력하여 수신 받는것으로 인식한다.
-	
-	
+
 }
--- Phone/src/phone/PhoneMain.java
+++ Phone/src/phone/PhoneMain.java
@@ -1,6 +1,35 @@
 package phone;
 
+import java.util.Scanner;
+
 public class PhoneMain {
-	Phone phone = new Phone();
-	
+	public static void main(String[] args) {
+		Phone phone = new Phone("아이폰14", "검정");
+		System.out.println("구매하시려는 제품을 선택해주세요.");
+		System.out.println("1번 = 1세대폰, 2번 = 2세대 폰(dmb), 3번 = 3세대 폰(스마트폰), 4번 = 슈퍼폰, 0번 = 취소");
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			String selPhone = sc.nextLine();
+			if (selPhone.equals("1")) {
+				System.out.println("제품명 :" + phone.model + " 색상 :" + phone.color + "을 지급합니다.");
+				phone.stateOff();
+				break;
+			} else if (selPhone.equals("2")) {
+				
+				break;
+			} else if (selPhone.equals("3")) {
+				
+				break;
+			} else if (selPhone.equals("4")) {
+				
+				break;
+			} else if (selPhone.equals("0")) {
+				System.out.println("취소를 선택하셨습니다. 종료합니다.");
+				break;
+			} else {
+				System.out.println("잘못 입력하셨습니다. 정확한 숫자를 입력해주세요.");
+				continue;
+			}
+		}
+	}
 }
