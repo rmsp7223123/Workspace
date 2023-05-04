909c230ef494bfc1b8e8f95a37207ee4f8bbbef6
rmsp7223123 <129022648+rmsp7223123@users.noreply.github.com>
2023-05-04 18:15:35 +0900

    문제풀이 ++

--- 999.Test/src/test2/b5.java
+++ 999.Test/src/test2/b5.java
@@ -12,33 +12,51 @@ public class b5 {
 		int sum = 0;
 		int sumA = 0;
 		int sumB = 0;
-		if (dataA > dataB) {
-			System.out.println("data A : " + dataA);
-			System.out.println("data B : " + dataB);
-			for (int i = 1; i <= dataB; i++) {
-				sumB += i;
-			}
-			for (int i = 1; i <= dataA; i++) {
-				sumA += i;
-			}
-			sum = sumB + sumA;
-			System.out.println("총합은 : " + sum);
-
-		} else if (dataA < dataB) {
-			System.out.println("data A : " + dataA);
-			System.out.println("data B : " + dataB);
-			for (int i = 1; i <= dataB; i++) {
-				sumB += i;
-			}
-			for (int i = 1; i <= dataA; i++) {
-				sumA += i;
-			}
-			sum = sumB + sumA;
-			System.out.println("총합은 : " + sum);
-
-		} else {
-			sum = dataA + dataB;
-			System.out.println("총합 : " + sum);
+		// dataA 10 : dataB : 8
+		// 8             10
+		if(dataA > dataB) {
+			int tempInt = dataA;
+			dataA = dataB;
+			dataB = tempInt;
 		}
+		for (int i = dataA; i <= dataB; i++) {
+			if(i == dataB) {
+				System.out.print(i + " = ");
+			}else {
+				System.out.print(i + " + ");				
+			}
+			sum+=i;
+		}
+		System.out.println(sum);
+		
+		
+//		if (dataA > dataB) {
+//			System.out.println("data A : " + dataA);
+//			System.out.println("data B : " + dataB);
+//			for (int i = 1; i <= dataB; i++) {
+//				sumB += i;
+//			}
+//			for (int i = 1; i <= dataA; i++) {
+//				sumA += i;
+//			}
+//			sum = sumB + sumA;
+//			System.out.println("총합은 : " + sum);
+//
+//		} else if (dataA < dataB) {
+//			System.out.println("data A : " + dataA);
+//			System.out.println("data B : " + dataB);
+//			for (int i = 1; i <= dataB; i++) {
+//				sumB += i;
+//			}
+//			for (int i = 1; i <= dataA; i++) {
+//				sumA += i;
+//			}
+//			sum = sumB + sumA;
+//			System.out.println("총합은 : " + sum);
+//
+//		} else {
+//			sum = dataA + dataB;
+//			System.out.println("총합 : " + sum);
+//		}
 	}
 }
